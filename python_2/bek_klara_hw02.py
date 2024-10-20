ab38a20912387d8a0d0eebedd1b251e377d6d792
KlaraBek <sadecka.kl@gmail.com>
2024-10-20 21:42:12 +0200

    drobne upravy formatovani

--- python_2/bek_klara_hw02.py
+++ python_2/bek_klara_hw02.py
@@ -6,7 +6,7 @@ cast = 'cast'
 genres = 'genres'
 decade = 'decade'
 
-seznam_dict = []  # list slovníků
+seznam_dict = []
 
 with open(r'python_2\netflix_titles.tsv', mode='r', encoding='utf-8') as netflix_file:
     header = netflix_file.readline().strip().split('\t')
@@ -19,12 +19,13 @@ with open(r'python_2\netflix_titles.tsv', mode='r', encoding='utf-8') as netflix
 
     for row in netflix_file:
         info = row.strip().split('\t')
-        info_dict = {title: info[title_idx],
-                     directors: (info[directors_idx]).split(", ") if info[directors_idx] else [],
-                     cast: (info[cast_idx]).split(", ") if (info[cast_idx]) else [],
-                     genres: (info[genres_idx]).split(","),
-                     decade: (info[startyear_idx])[:3] + '0',
-                     }
+        info_dict = {
+            title: info[title_idx],
+            directors: (info[directors_idx]).split(", ") if info[directors_idx] else [],
+            cast: (info[cast_idx]).split(", ") if (info[cast_idx]) else [],
+            genres: (info[genres_idx]).split(","),
+            decade: (info[startyear_idx])[:3] + '0',
+        }
         seznam_dict.append(info_dict)
 
 with open(r'python_2\hw02_output.json', mode='w', encoding='utf-8') as jsn_output_file:
